e9da87b28712079f95c6e057e3cdef8788be6b14
Sean Zweifel <spzweifel@gmail.com>
2023-05-07 17:45:27 -0400

    saved notes

--- server.js
+++ server.js
@@ -10,19 +10,16 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// three middleware we always want to use
 app.use(express.static('public'));
-
+app.use(express.json())
+app.use(express.urlencoded({
+  extended : true}))
 // WHEN I open the Note Taker FINISHED
 // THEN I am presented with a landing page with a link to a notes page
 // use GET /notes to return the notes.html file
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-});
 //post route. look up the lecture that covers this in lesson 17
 
 
@@ -35,14 +32,14 @@ app.get('*', (req, res) => {
 // use GET /api/notes which should read the db.json file and return the saved notes as JSON. I'm pretty sure this should display the saved notes as described above.
 app.get("/api/notes", (req, res) => {
   console.log(`${req.method} request received for notes`);
-  fs.readFile("./db/db.json").then((notes) => res.json(JSON.parse(notes)));
+  fs.readFile("./db/db.json", (err, notes) => res.json(JSON.parse(notes)));
 });
 
-fs.writeFile('./db/db.json', `${req.method}`, (err) =>
-err
-  ? console.error(err) : console.log('error found'));
+// fs.writeFile('./db/db.json', `${req.method}`, (err) =>
+// err
+//   ? console.error(err) : console.log('error found'));
 
-// WHEN I click on the Save icon
+// WHEN I click on the Save icon FINISHED
 // THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
 // this new note should be added to the db.json file. use POST /api/notes to do this. look into npm packages that could give each note a unique id when it's saved
 app.post('/api/notes', (req, res) => {
@@ -53,10 +50,18 @@ app.post('/api/notes', (req, res) => {
       title,
       text,
     }
+    fs.readFile("./db/db.json", (err, data) => {
+      var oldNotes = JSON.parse(data)
+      oldNotes.push(newNote)
+    
+    fs.writeFile("./db/db.json", JSON.stringify(oldNotes), (err) => {
+      res.json(oldNotes)
+    })
+  })
   }
 })
 
-// WHEN I click on an existing note in the list in the left-hand column
+// WHEN I click on an existing note in the list in the left-hand column FINISHED
 // THEN that note appears in the right-hand column
 //get route with a parameter. an index. also need a way to clean out what's already there too. could use a data attribute 
 
@@ -65,6 +70,12 @@ app.post('/api/notes', (req, res) => {
 //
 
 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
 
 app.listen(3001, () => console.log(`App listening at http://localhost:${PORT}`));
